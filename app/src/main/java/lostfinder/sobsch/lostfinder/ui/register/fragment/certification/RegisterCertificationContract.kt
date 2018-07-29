150b70d2140669e28f11ef9765e86199f094aec0
LeeHoYong <lee199402@gmail.com>
2018-07-29 15:49:46 +0900

    * 회원가입 기본화면 margin값 수정 * 아이핀, 핸드폰 이미지 적용

--- app/src/main/java/lostfinder/sobsch/lostfinder/ui/register/fragment/certification/RegisterCertificationContract.kt
+++ app/src/main/java/lostfinder/sobsch/lostfinder/ui/register/fragment/certification/RegisterCertificationContract.kt
@@ -1,15 +1,21 @@
 package lostfinder.sobsch.lostfinder.ui.register.fragment.certification
 
+import android.content.Context
+import android.widget.ImageView
 import lostfinder.sobsch.lostfinder.ui.base.BasePresenter
 import lostfinder.sobsch.lostfinder.ui.base.BaseView
 
-interface RegisterCertificationContract{
+interface RegisterCertificationContract {
 
-    interface View: BaseView{
+    interface View : BaseView {
+
+        fun phoneLogo(): ImageView
+        fun ipinLogo(): ImageView
 
     }
 
-    interface Presenter: BasePresenter<View>{
+    interface Presenter : BasePresenter<View> {
 
+        fun loadImage(context: Context)
     }
 }
