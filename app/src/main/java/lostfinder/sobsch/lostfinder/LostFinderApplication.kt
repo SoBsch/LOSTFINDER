34bf6b79ed456261e79852581ca25a41d342fa96
LeeHoYong <lee199402@gmail.com>
2018-07-30 18:26:46 +0900

    자동로그인 SharedPreference 추가

--- app/src/main/java/lostfinder/sobsch/lostfinder/LostFinderApplication.kt
+++ app/src/main/java/lostfinder/sobsch/lostfinder/LostFinderApplication.kt
@@ -2,15 +2,21 @@ package lostfinder.sobsch.lostfinder
 
 import android.app.Application
 import com.squareup.leakcanary.LeakCanary
+import lostfinder.sobsch.lostfinder.model.SharedPreference
 
-class LostFinderApplication: Application(){
+class LostFinderApplication : Application() {
 
+    companion object {
+
+        lateinit var prefs: SharedPreference
+    }
 
     override fun onCreate() {
         super.onCreate()
 
         appLeakcanary()
 
+        prefs = SharedPreference(applicationContext)
     }
 
     private fun appLeakcanary() {
