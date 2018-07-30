29f2180a1b80d314ef51acb1e022e50aeda8f157
LeeHoYong <lee199402@gmail.com>
2018-07-30 17:24:37 +0900

    * 본인인증 페이지 변경

--- app/src/main/java/lostfinder/sobsch/lostfinder/ui/register/fragment/certification/RegisterCertificationContract.kt
+++ app/src/main/java/lostfinder/sobsch/lostfinder/ui/register/fragment/certification/RegisterCertificationContract.kt
@@ -1,7 +1,5 @@
 package lostfinder.sobsch.lostfinder.ui.register.fragment.certification
 
-import android.content.Context
-import android.widget.ImageView
 import lostfinder.sobsch.lostfinder.ui.base.BasePresenter
 import lostfinder.sobsch.lostfinder.ui.base.BaseView
 
@@ -9,13 +7,9 @@ interface RegisterCertificationContract {
 
     interface View : BaseView {
 
-        fun phoneLogo(): ImageView
-        fun ipinLogo(): ImageView
-
     }
 
     interface Presenter : BasePresenter<View> {
 
-        fun loadImage(context: Context)
     }
 }
