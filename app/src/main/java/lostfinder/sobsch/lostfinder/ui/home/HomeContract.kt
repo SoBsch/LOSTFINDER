cfb09aad07d65fff60baecaf70a15712ae1276a8
LeeHoYong <lee199402@gmail.com>
2018-07-30 16:11:59 +0900

    #Add Home
    * 레이아웃 적용

--- app/src/main/java/lostfinder/sobsch/lostfinder/ui/home/HomeContract.kt
+++ app/src/main/java/lostfinder/sobsch/lostfinder/ui/home/HomeContract.kt
@@ -1,15 +1,23 @@
 package lostfinder.sobsch.lostfinder.ui.home
 
+import android.content.Context
+import android.media.Image
+import android.widget.ImageView
 import lostfinder.sobsch.lostfinder.ui.base.BasePresenter
 import lostfinder.sobsch.lostfinder.ui.base.BaseView
 
-interface HomeContract{
+interface HomeContract {
 
-    interface View: BaseView{
+    interface View : BaseView {
+
+        fun presentImage(): ImageView
+        fun messageImage(): ImageView
+        fun yourImage(): ImageView
 
     }
 
-    interface Presenter: BasePresenter<View>{
+    interface Presenter : BasePresenter<View> {
 
+        fun loadImages(context: Context)
     }
 }
--- app/src/main/java/lostfinder/sobsch/lostfinder/ui/home/HomePresenter.kt
+++ app/src/main/java/lostfinder/sobsch/lostfinder/ui/home/HomePresenter.kt
@@ -1,7 +1,15 @@
 package lostfinder.sobsch.lostfinder.ui.home
 
+import android.content.Context
+import lostfinder.sobsch.lostfinder.R
 import lostfinder.sobsch.lostfinder.ui.base.BasePresenterImpl
+import lostfinder.sobsch.lostfinder.util.ImageUtil
 
-class HomePresenter: BasePresenterImpl<HomeContract.View>(), HomeContract.Presenter{
+class HomePresenter : BasePresenterImpl<HomeContract.View>(), HomeContract.Presenter {
 
+    override fun loadImages(context: Context) {
+        ImageUtil.squareDrawableImage(mView!!.presentImage(), R.drawable.home_present, context)
+        ImageUtil.squareDrawableImage(mView!!.messageImage(), R.drawable.show_me_your_heart, context)
+        ImageUtil.squareDrawableImage(mView!!.yourImage(), R.drawable.home_text, context)
+    }
 }
