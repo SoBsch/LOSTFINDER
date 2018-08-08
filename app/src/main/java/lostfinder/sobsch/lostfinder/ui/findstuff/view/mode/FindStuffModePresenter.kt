e4db9c1ba9d6f039ac5f813f527c94590cf3c2e2
lhoyong <lee199402@gmail.com>
2018-08-08 16:10:48 +0900

    * 다이얼로그 수정

--- app/src/main/java/lostfinder/sobsch/lostfinder/ui/findstuff/view/mode/FindStuffModePresenter.kt
+++ app/src/main/java/lostfinder/sobsch/lostfinder/ui/findstuff/view/mode/FindStuffModePresenter.kt
@@ -2,25 +2,29 @@ package lostfinder.sobsch.lostfinder.ui.findstuff.view.mode
 
 import android.content.Context
 import lostfinder.sobsch.lostfinder.ui.base.BasePresenterImpl
+import lostfinder.sobsch.lostfinder.ui.chat.Chat
 import lostfinder.sobsch.lostfinder.ui.findstuff.FindStuffContract
 import lostfinder.sobsch.lostfinder.util.DIALOG_MODE
 import lostfinder.sobsch.lostfinder.util.DialogUtils
+import org.jetbrains.anko.intentFor
 
 class FindStuffModePresenter : BasePresenterImpl<FindStuffModeContract.View>(), FindStuffModeContract.Presenter {
 
     override fun openDirect(context: Context, isDirectOpen: FindStuffContract.FragmentCallbackListener) {
-        openDialog(context, isDirectOpen.onCamera())
+        openDialog(context)
     }
 
     override fun openMessage(context: Context, isMessageOpen: FindStuffContract.FragmentCallbackListener) {
-        openDialog(context, isMessageOpen.onCamera())
+        openDialog(context)
     }
 
-    private fun openDialog(context: Context, action: Unit) {
+    private fun openDialog(context: Context) {
 
         val dialog = DialogUtils.StuffNFCDialog(context, DIALOG_MODE)
 
         dialog.show()
-        dialog.setOnDismissListener { }
+        dialog.setOnDismissListener { intentMessage(context) }
     }
+
+    private fun intentMessage(context: Context) = context.startActivity(context.intentFor<Chat>())
 }
