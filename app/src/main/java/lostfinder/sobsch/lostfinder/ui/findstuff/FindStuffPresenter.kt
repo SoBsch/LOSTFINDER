c8fc4df4df85c1986cd9f9d7e1650dbe0cff292f
lhoyong <lee199402@gmail.com>
2018-08-08 15:07:05 +0900

    * 카메라 촬영시 전체화면 코드 변경

--- app/src/main/java/lostfinder/sobsch/lostfinder/ui/findstuff/FindStuffPresenter.kt
+++ app/src/main/java/lostfinder/sobsch/lostfinder/ui/findstuff/FindStuffPresenter.kt
@@ -2,12 +2,12 @@ package lostfinder.sobsch.lostfinder.ui.findstuff
 
 import android.app.Activity
 import android.support.v4.app.FragmentManager
-import android.view.WindowManager
 import lostfinder.sobsch.lostfinder.R
 import lostfinder.sobsch.lostfinder.ui.base.BasePresenterImpl
+import lostfinder.sobsch.lostfinder.ui.findstuff.view.camera.FindStuffCamera
 import lostfinder.sobsch.lostfinder.ui.findstuff.view.mode.FindStuffMode
 import lostfinder.sobsch.lostfinder.ui.findstuff.view.nfc.FindStuffNFC
-import lostfinder.sobsch.lostfinder.ui.stuff.fragment.camera.StuffCamera
+import lostfinder.sobsch.lostfinder.util.UIUtils
 
 class FindStuffPresenter : BasePresenterImpl<FindStuffContract.View>(), FindStuffContract.Presenter {
 
@@ -31,10 +31,10 @@ class FindStuffPresenter : BasePresenterImpl<FindStuffContract.View>(), FindStuf
 
     override fun onLoadCamera(activity: Activity) {
 
-        isViewFullScreen(true, activity)
+        UIUtils.isViewFullScreen(true, activity)
 
         supportFragmentMananger.beginTransaction()
-                .replace(container, StuffCamera())
+                .replace(container, FindStuffCamera())
                 .commit()
     }
 
@@ -43,14 +43,5 @@ class FindStuffPresenter : BasePresenterImpl<FindStuffContract.View>(), FindStuf
         supportFragmentMananger.beginTransaction()
                 .replace(container, FindStuffMode())
                 .commit()
-    }
-
-    private fun isViewFullScreen(isFull: Boolean, activity: Activity) {
-
-        if (isFull) {
-            activity.window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
-        } else {
-            activity.window.clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
-        }
     }
 }
