63762c4fe813736fbc1040c03b58b4c364a3b263
leehoyong <lee199402@gmail.com>
2018-08-21 06:14:15 -0300

    * nfc 가능여부 체크

--- app/src/main/java/lostfinder/sobsch/lostfinder/ui/stuff/fragment/nfcscan/StuffNfcScan.kt
+++ app/src/main/java/lostfinder/sobsch/lostfinder/ui/stuff/fragment/nfcscan/StuffNfcScan.kt
@@ -1,5 +1,7 @@
 package lostfinder.sobsch.lostfinder.ui.stuff.fragment.nfcscan
 
+import android.nfc.NfcAdapter
+import android.util.Log
 import android.widget.ImageView
 import kotlinx.android.synthetic.main.stuff_nfc_scan.*
 import lostfinder.sobsch.lostfinder.R
@@ -20,10 +22,24 @@ class StuffNfcScan : BaseFragment<StuffNfcScanContract.View, StuffNfcScanContrac
     override fun init() {
 
         mPresenter.loadImage(context!!)
+        mPresenter.setNfcAdapter(context!!)
 
         // nfc click
         stuff_nfc_scan_submit.setOnClickListener {
-            mPresenter.nfcSuccess(context!!, mCallback)
+            // mPresenter.nfcSuccess(context!!, mCallback)
+            test()
+        }
+
+    }
+
+    private fun test() {
+        var adapter = NfcAdapter.getDefaultAdapter(context!!)
+
+        if(adapter == null){
+            Log.e("nfc", "지원 안하는 단말기")
+            return
+        }else{
+            Log.e("nfc", "지원 함")
         }
 
     }
