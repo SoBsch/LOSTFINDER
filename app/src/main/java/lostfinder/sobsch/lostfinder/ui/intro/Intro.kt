98a7030ecd8f15b3bbdc09b3f6033b26cda85e7c
leehoyong <lee199402@gmail.com>
2018-08-22 16:17:20 +0900

    * gif
    * xml fix

--- app/src/main/java/lostfinder/sobsch/lostfinder/ui/intro/Intro.kt
+++ app/src/main/java/lostfinder/sobsch/lostfinder/ui/intro/Intro.kt
@@ -1,6 +1,8 @@
 package lostfinder.sobsch.lostfinder.ui.intro
 
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
 import kotlinx.android.synthetic.main.activity_intro.*
 import lostfinder.sobsch.lostfinder.R
 import lostfinder.sobsch.lostfinder.ui.base.BaseActivity
@@ -12,9 +14,11 @@ class Intro : BaseActivity<IntroContract.View, IntroContract.Presenter>(), Intro
 
     override fun init() {
 
+        // TODO gif 이미지 bottom 빈공간 남는거
         Glide.with(this)
                 .asGif()
                 .load(R.drawable.phone_gif)
+                .apply(RequestOptions.diskCacheStrategyOf(DiskCacheStrategy.NONE))
                 .into(intro_top_gif)
 
     }
